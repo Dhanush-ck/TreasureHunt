fe3cd494aa1014dc4e01a9a3db442ed3812f3cf2
Dhanush C K <dhanushck01@gmail.com>
2025-12-31 23:01:08 +0530

    feat: setup login

---
+++ public/js/login.js
@@ -0,0 +1,71 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, set, get, orderByChild, equalTo, query  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxFLQifojzsNjc98vCUPac8IYKzcfrneM",
+  authDomain: "treasurehuntv26.firebaseapp.com",
+  databaseURL: "https://treasurehuntv26-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "treasurehuntv26",
+  storageBucket: "treasurehuntv26.firebasestorage.app",
+  messagingSenderId: "824182490010",
+  appId: "1:824182490010:web:8d145dd3bc2411d6677023"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const submitBtn = document.getElementById('submit-btn');
+
+const errorMessage = document.querySelector('.error');
+
+submitBtn.addEventListener('click', login);
+
+function login() {
+  const username = document.getElementById('name').value;
+  const password = document.getElementById('password').value;
+  if(!username || username == " ") {
+    errorMessage.innerHTML = "Name can't be empty";
+    messagePopup();
+    return;
+  }
+
+  if(!password || password == " ") {
+    errorMessage.innerHTML = "Password can't be empty";
+    messagePopup();
+    return;
+  }
+  
+  const userRef = ref(db, "users/" + username);
+
+  get(userRef).then((snapshot) => {
+    if (!snapshot.exists()) {
+      console.log("User not found");
+      errorMessage.innerHTML = "User not found";
+      messagePopup();
+    } else {
+      const data = snapshot.val();
+      if (data.password === password) {
+        localStorage.setItem("username", username);
+        localStorage.setItem("level", data.currentLevel);
+        console.log("Login Successfull");
+        // window.location.href = "game.html";
+      } else {
+        console.log("Wrong Password");
+        errorMessage.innerHTML = "Wrong Password";
+        messagePopup();
+      }
+    }
+  });
+}
+
+function messagePopup() {
+    if(errorMessage.classList.contains('fadeOut')){
+        errorMessage.classList.remove('fadeOut');
+    }
+    errorMessage.classList.add('fadeIn');
+    setTimeout(() => {
+        errorMessage.classList.remove('fadeIn');
+        errorMessage.classList.add('fadeOut');
+    }, 2000);
+}
